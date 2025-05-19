0daa8b2b7d089525e633d3f1c5ce855879816b01
RielleTatel <tatelgabrielle19@gmail.com>
2025-05-19 15:35:24 +0800

    Initial Commit

---
+++ FINAL-SOURCE-FILE/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FINAL-SOURCE-FILE
+//
+//  Created by Gabrielle Tatel on 5/19/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
